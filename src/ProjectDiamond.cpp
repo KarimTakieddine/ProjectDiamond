e4d6a78eaacca62d6d5a85333241db8e8f91e36c
MounirTakieddine <MounirTakieddine@bitbucket.org>
2023-07-10 21:01:38 +0100

    Update submodule: "DiamondEngine" and provide source implementation

--- src/ProjectDiamond.cpp
+++ src/ProjectDiamond.cpp
@@ -1,7 +1,32 @@
 #pragma once
 
+#include <diamond_engine/GraphicsContext.h>
 #include <diamond_engine/LogManager.h>
+#include <diamond_engine/SceneLoader.h>
 
 int main(int argc, char** argv) {
+	try {
+		std::unique_ptr<diamond_engine::GraphicsContext> graphicsContext = std::make_unique<diamond_engine::GraphicsContext>();
+		graphicsContext->InitializeWindow({ 1280, 960 }, "Hello!");
+		graphicsContext->InitializeGLEW();
+
+		std::shared_ptr<diamond_engine::SharedShaderStore> sharedShaderStore = std::make_shared<diamond_engine::SharedShaderStore>();
+		sharedShaderStore->Load("shaders");
+
+		std::shared_ptr<diamond_engine::SceneLoader> sceneLoader = std::make_shared<diamond_engine::SceneLoader>();
+		sceneLoader->SetSharedShaderStore(sharedShaderStore);
+		sceneLoader->LoadScenes("scenes");
+
+		diamond_engine::Scene* sampleScene = sceneLoader->FindScene("sample_scene");
+		graphicsContext->SetScene(sampleScene);
+
+		graphicsContext->Execute();
+	}
+	catch (const std::exception& e) {
+		LOG_ERROR(e.what());
+
+		std::this_thread::sleep_for(std::chrono::seconds(10));
+	}
+
 	return 0;
 }
