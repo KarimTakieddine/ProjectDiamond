606f892a61b17055d107c8d854288bfea0818872
MounirTakieddine <MounirTakieddine@bitbucket.org>
2023-08-19 00:09:25 +0100

    Update submodule commit. Make use of EngineConfiguration when initializing graphics context

--- src/ProjectDiamond.cpp
+++ src/ProjectDiamond.cpp
@@ -5,21 +5,18 @@
 #include <diamond_engine/scene/SceneLoader.h>
 #include <diamond_engine/shader/SharedShaderStore.h>
 #include <diamond_engine/texture/TextureLoader.h>
-#include <diamond_engine/input/Input.h>
+#include <diamond_engine/parser/EngineConfigParser.h>
 
 int main(int argc, char** argv) {
 	try {
 		std::unique_ptr<diamond_engine::GraphicsContext> graphicsContext = std::make_unique<diamond_engine::GraphicsContext>();
-		graphicsContext->InitializeWindow({ 1280, 960 }, "Hello!");
-		graphicsContext->InitializeGLEW();
+		graphicsContext->Initialize(diamond_engine::EngineConfigParser::ParseFromFile("config/engineConfig.xml"));
 
 		std::shared_ptr<diamond_engine::SharedShaderStore> sharedShaderStore = std::make_shared<diamond_engine::SharedShaderStore>();
 		sharedShaderStore->Load("shaders");
 
 		std::shared_ptr<diamond_engine::TextureLoader> sharedTextureLoader = std::make_shared<diamond_engine::TextureLoader>();
 		sharedTextureLoader->Load("textures");
-
-		diamond_engine::input::StateMonitor::GetInstance().RegisterKeyboardKey("Space", diamond_engine::input::GLFWKeyCode::KEYCODE_SPACE);
 
 		std::shared_ptr<diamond_engine::SceneLoader> sceneLoader = std::make_shared<diamond_engine::SceneLoader>();
 		sceneLoader->SetSharedShaderStore(sharedShaderStore);
