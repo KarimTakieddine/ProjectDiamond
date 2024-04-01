1316fe96302ce540f201601b850ce94185202de6
KarimTakieddine <karim.m.takieddine@gmail.com>
2024-04-01 16:37:39 +0100

    Initial push to branch

--- src/ProjectDiamond.cpp
+++ src/ProjectDiamond.cpp
@@ -1,10 +1,12 @@
 #pragma once
 
+/*
 #include <diamond_engine/animation/SpriteSheetLoader.h>
 #include <diamond_engine/audio/AudioEngine.h>
 #include <diamond_engine/behaviour/BehaviourRegistry.h>
 #include <diamond_engine/engine/GraphicsContext.h>
 #include <diamond_engine/parser/EngineConfigParser.h>
+#include <diamond_engine/rendering/GameInstanceManager.h>
 #include <diamond_engine/scene/SceneLoader.h>
 #include <diamond_engine/shader/SharedShaderStore.h>
 #include <diamond_engine/texture/TextureLoader.h>
@@ -13,22 +15,38 @@
 #include "behaviour/CharacterController2DBehaviour.h"
 #include "builder/CharacterController2DBehaviourBuilder.h"
 #include "parser/CharacterController2DBehaviourConfigParser.h"
+*/
+
+#include <diamond_engine/engine/GraphicsContext.h>
+#include <diamond_engine/parser/EngineConfigParser.h>
+#include <diamond_engine/rendering/GameEngine.h>
+#include <diamond_engine/utility/LogManager.h>
 
 int main(int argc, char** argv) {
 	try {
-		diamond_engine::RegisterBehaviour("CharacterController2DBehaviour", &CharacterController2DBehaviourConfigParser::Parse, &CharacterController2DBehaviourBuilder::Build);
+		//diamond_engine::RegisterBehaviour("CharacterController2DBehaviour", &CharacterController2DBehaviourConfigParser::Parse, &CharacterController2DBehaviourBuilder::Build);
 
-		diamond_engine::AudioEngine::instance()->initialize("audio");
+		//diamond_engine::AudioEngine::instance()->initialize("audio");
 
-		std::unique_ptr<diamond_engine::GraphicsContext> graphicsContext = std::make_unique<diamond_engine::GraphicsContext>();
-		graphicsContext->Initialize(diamond_engine::EngineConfigParser::ParseFromFile("config/engineConfig.xml"));
+		std::unique_ptr<diamond_engine::GameEngine> gameEngine = std::make_unique<diamond_engine::GameEngine>();
+		gameEngine->initialize(diamond_engine::EngineConfigParser::ParseFromFile("./config/engineConfig.xml"));
+		gameEngine->addScene("testScene", "./scenes/testScene.xml");
+		gameEngine->loadScene("testScene");
+		gameEngine->loadScene("testScene");
+		gameEngine->loadScene("testScene");
+		gameEngine->loadScene("testScene");
+		gameEngine->loadScene("testScene");
+		gameEngine->run();
+		gameEngine->unloadCurrentScene();
 
-		std::shared_ptr<diamond_engine::SharedShaderStore> sharedShaderStore = std::make_shared<diamond_engine::SharedShaderStore>();
-		sharedShaderStore->Load("shaders");
+		/*
+		diamond_engine::SceneConfig sceneConfig;
+		sceneConfig.SetMaxObjects(1);
+		gameInstanceManager->loadScene(sceneConfig);
+		gameInstanceManager->loadScene(sceneConfig);
+		*/
 
-		std::shared_ptr<diamond_engine::TextureLoader> sharedTextureLoader = std::make_shared<diamond_engine::TextureLoader>();
-		sharedTextureLoader->Load("textures");
-
+		/*
 		std::shared_ptr<diamond_engine::SpriteSheetLoader> sharedSpriteSheetLoader = std::make_shared<diamond_engine::SpriteSheetLoader>();
 		sharedSpriteSheetLoader->setSharedTextureLoader(sharedTextureLoader);
 		sharedSpriteSheetLoader->load("spriteSheets");
@@ -43,6 +61,7 @@ int main(int argc, char** argv) {
 		graphicsContext->SetScene(sampleScene);
 
 		graphicsContext->Execute();
+		*/
 	}
 	catch (const std::exception& e) {
 		LOG_ERROR(e.what());
